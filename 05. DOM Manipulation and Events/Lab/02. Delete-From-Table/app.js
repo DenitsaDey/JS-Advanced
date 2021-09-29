5a85a64f53cb5c2576dbf7e85ba69b949b4ad24e
Denitsa <denitsa.dey@gmail.com>
2021-09-29 11:24:41 +0300

    DOM Events Lab Solution

--- 05. DOM Manipulation and Events/Lab/02. Delete-From-Table/app.js
+++ 05. DOM Manipulation and Events/Lab/02. Delete-From-Table/app.js
@@ -1,3 +1,10 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    const input = document.querySelector('input[name="email"]');
+
+    const rows = Array.from(document.querySelector('tbody').children)
+    .filter(r => r.children[1].textContent == input.value);
+
+    rows.forEach(r => r.remove());
+
+    document.getElementById('result').textContent = rows.length > 0 ? 'Deleted.' : 'Not found.';
 }

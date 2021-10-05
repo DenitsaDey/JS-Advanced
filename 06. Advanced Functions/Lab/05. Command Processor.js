36930ef74dec9cbf16cf9222cb4c4128e6f81219
Denitsa <denitsa.dey@gmail.com>
2021-10-05 04:52:53 +0300

    Advanced Functions Lab Solutions

---
+++ 06. Advanced Functions/Lab/05. Command Processor.js
@@ -0,0 +1,25 @@
+function solve(){
+    let str = '';
+    return {
+        append: (s) => str += s,
+        removeStart: (n) => str = str.substring(n),
+        removeEnd: (n) => str = str.substring(0, str.length -n),
+        print: () => console.log(str)
+    }
+}
+
+let firstZeroTest = solve();
+
+firstZeroTest.append('hello');
+firstZeroTest.append('again');
+firstZeroTest.removeStart(3);
+firstZeroTest.removeEnd(4);
+firstZeroTest.print();
+
+let secondZeroTest = solve();
+
+secondZeroTest.append('123');
+secondZeroTest.append('45');
+secondZeroTest.removeStart(2);
+secondZeroTest.removeEnd(1);
+secondZeroTest.print();

1809198f49d2976ae023bbf4fa466c1b1eb4d962
Denitsa <denitsa.dey@gmail.com>
2021-10-15 05:34:11 +0300

    Prototypes and Inheritance Lab Solutions

--- 08. Classes/Exercise/test/12. Payment Package.test.js
+++ 08. Classes/Exercise/test/12. Payment Package.test.js
@@ -3,11 +3,13 @@ const assert = require('chai').assert;
 const describe = require('mocha').describe;
 
 describe('Test paymentPackage functionality', () => {
-    let instance = undefined;
-    beforeEach(() => {
-        instance = new paymentPackage('Name', 100);
-    })
+    // let instance = undefined;
+    // beforeEach(() => {
+    //     instance = new paymentPackage('Name', 100);
+    // })
     it('should create class using the constructor', () => {
+        let instance = new paymentPackage('Name', 100);
+
         assert.equal(Object(instance).hasOwnProperty('_name'), true);
         assert.equal(instance._name, 'Name');
         assert.equal(instance._value, 100);
@@ -16,6 +18,8 @@ describe('Test paymentPackage functionality', () => {
     })
 
     it('should pass when name is provided', () => {
+        let instance = new paymentPackage('Name', 100);
+
         assert.equal(instance.name, 'Name');
 
         instance.name = 'Pesho';
@@ -25,24 +29,34 @@ describe('Test paymentPackage functionality', () => {
     })
 
     it('should pass when value is provided', () => {
+        let instance = new paymentPackage('Name', 100); 
+
         assert.equal(instance.value, 100);
 
         instance.value = 200;
         assert.equal(instance.value, 200);
         assert.throw(() => {instance.value = -5}, 'Value must be a non-negative number');
         assert.throw(() => {instance.value = '2'}, 'Value must be a non-negative number');
+        assert.doesNotThrow(() => {instance.value = 0});
     })
 
     it('should pass when VAT is provided', () => {
+        let instance = new paymentPackage('Name', 100); 
+
         assert.equal(instance.VAT, 20);
 
         instance.VAT = 40;
         assert.equal(instance.VAT, 40);
         assert.throw(() => {instance.VAT = 'vat'}, 'VAT must be a non-negative number');
         assert.throw(() => {instance.VAT = -2}, 'VAT must be a non-negative number');
+        assert.throw(() => {instance.VAT = '2'}, 'VAT must be a non-negative number');
+        assert.throw(() => {instance.VAT = undefined}, 'VAT must be a non-negative number');
+        assert.doesNotThrow(() => {instance.VAT = 0});
     })
 
     it('should pass when active', () => {
+        let instance = new paymentPackage('Name', 100);
+
         assert.equal(instance.active, true);
 
         instance.active = false;
@@ -57,6 +71,7 @@ describe('Test paymentPackage functionality', () => {
         `- Value (excl. VAT): ${value}`,
         `- Value (VAT ${VAT}%): ${value * (1 + VAT / 100)}`].join('\n');
     }
+        let instance = new paymentPackage('Name', 100);
         
         assert.equal(instance.toString(), getString('Name', 100));
         instance.name = 'Gosho';
